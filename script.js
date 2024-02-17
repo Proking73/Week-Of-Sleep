0a0db81aea5256e71b32359faae4cfd343e17192
The person <77035338+Proking73@users.noreply.github.com>
2024-02-16 22:35:07 -0500

    Update script.js
    /images/BlackBG.png to images/BlackBG.png

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ if (theme == "light") {
     variables.style.setProperty('--lightMode', 'black');
     variables.style.setProperty('--darkMode', 'white');
     // variables.style.setProperty('--fontWeight', '600'); CHANGE THIS VALUE INTO SOMETHING
-    logo.src = "/images/WhiteBG.png";
+    logo.src = "images/WhiteBG.png";
     console.log("lightmode");
     console.log(logo.src)
 } else if (theme == "dark") {
@@ -16,7 +16,7 @@ if (theme == "light") {
     variables.style.setProperty('--darkMode', 'black');
     document.querySelector(".extras").style.filter = "invert(100%)";
     // variables.style.setProperty('--fontWeight', '400'); CHANGE THIS VALUE INTO SOMETHING
-    logo.src = "/images/BlackBG.png";
+    logo.src = "images/BlackBG.png";
     console.log("darkmode");
 } else {
     variables.style.setProperty('--lightMode', 'black');
