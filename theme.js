ce333a067779e988a6e9d97c03c2e6060de0be74
Proking73 HP <ishantgupta020@gmail.com>
2024-03-02 23:04:50 -0500

    NOT WORKING SWITCH

--- theme.js
+++ theme.js
@@ -19,31 +19,14 @@ function themeCheck() {
 
 function themeInitiate() {
     if (currentTheme == "light") {
-        variables.style.setProperty('--lightMode', 'black');
-        variables.style.setProperty('--darkMode', 'white');
-        document.querySelector("main").style.backgroundColor = " var(--darkMode)";
-    
-        daySections.forEach(day => {
-            day.style.boxShadow = " 0px 0px 2vmin 0.25vmin hsla(0, 0%, 50%, 0.5)";
-        });
-        // variables.style.setProperty('--fontWeight', '600'); CHANGE THIS VALUE INTO SOMETHING
-        logo.src = "images/WhiteBG.png"
-        theme = "light";
-    
-    } else if (currentTheme == "dark") {
-        variables.style.setProperty('--lightMode', 'white');
-        variables.style.setProperty('--darkMode', 'black');
-        document.querySelector(".extras").style.filter = "invert(100%)";
-        document.querySelector("main").style.backgroundColor = " var(--darkModeBG)";
-        document.querySelector(".locationName").style.backgroundColor = "var(--darkModeBG)";
-        
-        daySections.forEach(day => {
-            day.style.boxShadow = " 0px 0px 2vmin 0.25vmin black";
-        });
-        // variables.style.setProperty('--fontWeight', '400'); CHANGE THIS VALUE INTO SOMETHING
-        logo.src = "images/BlackBG.png";
-        theme = "dark";
+        light();
 
+    } else if (currentTheme == "dark") {
+        dark();
+
+    }
+    else {
+        light();
     }
 }
 
@@ -52,37 +35,45 @@ themeCheck();
 
 themeButton.addEventListener('click', () => {
     if (theme == "light") {
-        variables.style.setProperty('--lightMode', 'black');
-        variables.style.setProperty('--darkMode', 'white');
-        document.querySelector("main").style.backgroundColor = " var(--darkMode)";
-        
-        daySections.forEach(day => {
-            day.style.boxShadow = " 0px 0px 2vmin 0.25vmin hsla(0, 0%, 50%, 0.5)";
-        });
-
-        logo.src = "images/WhiteBG.png"
-        
-        theme = "dark";
-        console.log("light")
+        light();
         
     } else if (theme == "dark") {
-        variables.style.setProperty('--lightMode', 'white');
-        variables.style.setProperty('--darkMode', 'black');
-        document.querySelector(".extras").style.filter = "invert(100%)";
-        document.querySelector("main").style.backgroundColor = " var(--darkModeBG)";
-        document.querySelector(".locationName").style.backgroundColor = "var(--darkModeBG)";
-        
-        daySections.forEach(day => {
-            day.style.boxShadow = " 0px 0px 2vmin 0.25vmin black";
-        });
+        dark();
 
-        logo.src = "images/BlackBG.png";
-        
-        theme = "light";
-        console.log("dark")
-        
     }
     
     localStorage.setItem("theme", theme);
     themeCheck();
 });
+
+
+// Functions for light and dark modes
+function light() {
+    variables.style.setProperty('--lightMode', 'black');
+    variables.style.setProperty('--darkMode', 'white');
+    document.querySelector("main").style.backgroundColor = " var(--darkMode)";
+
+    daySections.forEach(day => {
+        day.style.boxShadow = " 0px 0px 2vmin 0.25vmin hsla(0, 0%, 50%, 0.5)";
+    });
+    // variables.style.setProperty('--fontWeight', '600'); CHANGE THIS VALUE INTO SOMETHING
+    logo.src = "images/WhiteBG.png"
+    theme = "light";
+
+};
+
+function dark() {
+    variables.style.setProperty('--lightMode', 'white');
+    variables.style.setProperty('--darkMode', 'black');
+    document.querySelector(".extras").style.filter = "invert(100%)";
+    document.querySelector("main").style.backgroundColor = " var(--darkModeBG)";
+    document.querySelector(".locationName").style.backgroundColor = "var(--darkModeBG)";
+    
+    daySections.forEach(day => {
+        day.style.boxShadow = " 0px 0px 2vmin 0.25vmin black";
+    });
+    // variables.style.setProperty('--fontWeight', '400'); CHANGE THIS VALUE INTO SOMETHING
+    logo.src = "images/BlackBG.png";
+    theme = "dark";
+    
+}
